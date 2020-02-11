b783e238f4776be6dafd66ac2627b62371b0538e
proxybee <inspireworldsyouth@gmail.com>
2020-02-11 13:51:39 -0800

    react boiler plate 1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+import React, { Component } from 'react';
+
+
+import './styles/App.css'
+
+class App extends Component {
+    render() {
+      return (
+        <div>
+          <h1>My React App!</h1>
+        </div>
+      )
+    }
+  }
+
+  // const App = () => {
+  //   return(
+  //     <div>
+  //       <h1>Hello React!</h1>
+  //     </div>
+  //   )
+  // }
+  
+  export default App;
